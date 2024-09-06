93aa0263797f17093c3eb995a4f62a3c1b0d902d
Jacob Sanders <sanders.jms@gmail.com>
2024-09-06 00:35:20 -0600

    add users table and user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_04_074849) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_06_063002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,5 +19,12 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_04_074849) do
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 end
