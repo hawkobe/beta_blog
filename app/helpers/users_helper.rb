fda74b53cb05f6159af8c93869c3784d27099003
Jacob Sanders <sanders.jms@gmail.com>
2024-09-09 11:44:44 -0600

    show action user account page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  def gravatar_for(user, options = { size: 80 })
+    email_address = user.email.downcase
+    hash = Digest::SHA256.hexdigest(email_address)
+    size = options[:size]
+    default = "https://www.gravatar.com/avatar/?s=#{size}"
+    params = URI.encode_www_form("d" => default, "s" => size)
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?#{params}"
+    image_tag(gravatar_url, alt: user.username, class: "rounded-circle shadow mx-auto d-block mt-3")
+  end
 end
