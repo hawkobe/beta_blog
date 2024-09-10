8fc7b034d1057801cc342798b6b67b0e5249f1a2
Jacob Sanders <sanders.jms@gmail.com>
2024-09-09 18:27:27 -0600

    db rollback

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  has_secure_password
   before_save { self.email = email.downcase }
   has_many :articles
   validates :username, presence: true,
@@ -10,4 +9,5 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false },
                     length: { maximum: 105 },
                     format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_07_174401) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_10_000653) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
